50381e490b32182a85c90b9045ff66581b69dbb4
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-05 23:34:41 +0530

    kafka cli done

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Intro to Kafka
 ## Installation Kafka (Mac)
 
-### Install kafka binary 
+### Install kafka 
 ```
 brew install kafka 
 ```
@@ -10,7 +10,30 @@ brew install kafka
 ```
 zookeeper-server-start config/zookeeper.properties
 ```
-## To start the kafka server
+### To start the kafka server
 ```
 kafka-server-start config/server.properties
+```
+
+
+## Kafka CLI
+
+Create the kafka topics (More in kafka-command-line-interface.sh file)
+```
+kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --create --partition 3 --replication-factor 1
+```
+
+List the kafka topics
+```
+kafka-topics --zookeeper 12.0.0.1:2181 --list
+```
+
+Describe the kafka topics
+```
+kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --describe
+```
+
+Delete the kafka topics
+```
+kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --delete
 ```
---
+++ kafka-command-line/kafka-commnad-line-interface.sh
@@ -0,0 +1,23 @@
+# Kafka CLI (Command Line Interface)
+
+# Create the topics in Kafka in (CLI)
+# --topic enter the name of the topic 
+# -- create  it will create the topic 
+# -- partition enter the number of partitions want
+# --replicaiton-factor enter the number of replication factor 
+# Note: brokerId=0  number of broker is 1 so the replication factor is max 1 
+kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --create --partition 3 --replication-factor 1
+
+
+# List the topics in kafka in (CLI)
+# it will show all the list of topic create
+kafka-topics --zookeeper 12.0.0.1:2181 --list
+
+
+# Describe the kafka topics
+# it will show the table 
+kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --describe
+
+# Delete the kafka topic
+# it will delete the kafka topic
+kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --delete
