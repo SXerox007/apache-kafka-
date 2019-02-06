089db731cce0441b6dcd728225b8670e8bc75f2d
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-06 15:33:08 +0530

    change producer and topic .sh file

--- kafka-command-line/kafka-cli-producer.sh
+++ kafka-command-line/kafka-cli-producer.sh
@@ -4,15 +4,25 @@ kafka-console-producer --broker-list 12.0.0.1:9092 --topic name_of_topic
 # > Enter any text
 # > hello
 
+# or
+kafka-console-producer.sh --broker-list 12.0.0.1:9092 --topic name_of_topic
+
+
 
 #  --producer-property acks-all
 kafka-console-producer --broker-list 12.0.0.1:9092 --topic name_of_topic --producer-property acks=all
+# or
+kafka-console-producer.sh --broker-list 12.0.0.1:9092 --topic name_of_topic --producer-property acks=all
+
 
 
 # If we enter the topic which are not exist it will default create topic by server.property file according
 # but say warning with the leader not available 
 # Note : Good for code if first create topic then do the kakfa-console-producer
 kafka-console-producer --broker-list 12.0.0.1:9092 --topic name_of_topic_not_exist
+# or
+kafka-console-producer.sh --broker-list 12.0.0.1:9092 --topic name_of_topic_not_exist
+
 
 # config/server.properties
 # we can set the number of partitions in there we want in default or change some other default properties in there
--- kafka-command-line/kafka-cli-topic.sh
+++ kafka-command-line/kafka-cli-topic.sh
@@ -8,17 +8,28 @@
 # Note: brokerId=0  number of broker is 1 so the replication factor is max 1 
 # By defalut replication factor is 1 we can change in server.properties file to defalut properties set while create the topic
 kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --create --partition 3 --replication-factor 1
+# or
+kafka-topics.sh --zookeeper 12.0.0.1:2181 --topic name_of_topic --create --partition 3 --replication-factor 1
+
 
 
 # List the topics in kafka in (CLI)
 # it will show all the list of topic create
 kafka-topics --zookeeper 12.0.0.1:2181 --list
+# or
+kafka-topics.sh --zookeeper 12.0.0.1:2181 --list
+
 
 
 # Describe the kafka topics
 # it will show the table 
 kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --describe
+# or
+kafka-topics.sh --zookeeper 12.0.0.1:2181 --topic name_of_topic --describe
+
 
 # Delete the kafka topic
 # it will delete the kafka topic
 kafka-topics --zookeeper 12.0.0.1:2181 --topic name_of_topic --delete
+# or
+kafka-topics.sh --zookeeper 12.0.0.1:2181 --topic name_of_topic --delete
