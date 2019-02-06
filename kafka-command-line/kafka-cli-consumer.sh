71ca68e9f9fcb6b9fb2a0b0fc7726613742758e9
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-02-06 23:22:04 +0530

    kafka cli consumers

---
+++ kafka-command-line/kafka-cli-consumer.sh
@@ -0,0 +1,6 @@
+# Kafka CLI consumers
+# Kafka console consumer  it will consumer the Real time topic messasge only
+kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic name_of_topic
+
+# For all the topic messages
+kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic name_of_topic --form-beginning
